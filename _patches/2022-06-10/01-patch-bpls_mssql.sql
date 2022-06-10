bfa65cc72cadbcd501dc0e1d92c2ad1182cb546a
worgflores <worgflores@gmail.com>
2022-06-10 01:30:03 +0000

    bpls: patch scripts

--- _patches/2022-06-10/01-patch-bpls_mssql.sql
+++ _patches/2022-06-10/01-patch-bpls_mssql.sql
@@ -1,3 +1,27 @@
 
 ALTER TABLE business ADD mainlobid varchar(50) NULL
 go 
+
+
+update aa set 
+	aa.mainlobid = bb.mainlobid 
+from 
+	business aa, 
+	( 
+		SELECT
+			 b.objid, 
+			 (
+				 SELECT TOP 1 lob_objid  
+					FROM business_active_info
+					WHERE businessid = b.objid  
+						AND attribute_name IN ( 'CAPITAL', 'GROSS' )
+						AND decimalvalue > 0 
+					ORDER BY decimalvalue DESC 
+			 ) AS mainlobid 
+		FROM business b
+		WHERE b.activeyear = 2022 
+	)bb 
+where 
+	aa.objid = bb.objid 
+
+go 
--- _patches/2022-06-10/01-patch-bpls_mysql.sql
+++ _patches/2022-06-10/01-patch-bpls_mysql.sql
@@ -1,3 +1,27 @@
 
 ALTER TABLE business ADD mainlobid varchar(50) NULL
 ;
+
+
+update 
+	business aa, 
+	( 
+		SELECT
+			 b.objid, 
+			 (
+				 SELECT lob_objid  
+					FROM business_active_info
+					WHERE businessid = b.objid  
+						AND attribute_name IN ( 'CAPITAL', 'GROSS' )
+						AND decimalvalue > 0 
+					ORDER BY decimalvalue DESC LIMIT 1
+			 ) AS mainlobid 
+		FROM business b
+		WHERE b.activeyear = 2022 
+	)bb 
+set
+	aa.mainlobid = bb.mainlobid
+where 
+	aa.objid = bb.objid 
+; 
+
