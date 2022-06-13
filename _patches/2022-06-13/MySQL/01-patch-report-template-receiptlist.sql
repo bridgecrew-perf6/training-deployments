623ccdf2d59633a2c7de109224632534514f9bfe
worgflores <worgflores@gmail.com>
2022-06-13 05:07:43 +0000

    new patches

---
+++ _patches/2022-06-13/MySQL/01-patch-report-template-receiptlist.sql
@@ -0,0 +1,11 @@
+INSERT INTO `sys_report_template` (`name`, `title`, `filepath`, `master`, `icon`) 
+VALUES ('receiptlist', 'Receipt List', 'reports/treasury/receipt/receiptlist.jasper', '0', NULL)
+;
+
+INSERT INTO `sys_report_def` (`name`, `title`, `category`, `template`, `reportheader`, `role`, `sortorder`, `statement`, `permission`, `parameters`, `querytype`, `state`, `description`, `properties`) 
+VALUES ('receiptlist', 'Receipt List', 'reports/treasury', 'receiptlist', NULL, NULL, '1', 'SELECT objid, receiptno, receiptdate, paidby, amount,\nCONCAT( ''qrcode:'', receiptno ) AS qrcode\n FROM cashreceipt LIMIT 50', NULL, '[]', 'SQL', 'DRAFT', NULL, '[]')
+;
+
+INSERT INTO `sys_report_subreport_def` (`objid`, `parentid`, `reportid`, `name`, `querytype`, `statement`) 
+VALUES ('SUBRPT-61a424b5:1814b346812:-71fb', NULL, 'receiptlist', 'items', 'SQL', 'SELECT \n    objid,\n    item_code,\n    item_title,\n    amount,\n    remarks\nFROM cashreceiptitem WHERE receiptid = $P{objid}')
+;
